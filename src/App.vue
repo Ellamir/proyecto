748787b38afe1e6d25dee3d0cfef674625cff5d6
Ellamir <tamara.fendez@gmail.com>
2020-06-14 19:00:53 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<<<<<<< HEAD
+<style lang="scss">
+=======
+<style>
+>>>>>>> 4ee511c53c2f44a87d16e33541d16bbb3490a8dc
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+<<<<<<< HEAD
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+=======
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+>>>>>>> 4ee511c53c2f44a87d16e33541d16bbb3490a8dc
+}
+</style>
