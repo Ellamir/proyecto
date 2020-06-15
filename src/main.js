32ee33f5143f19454944ba67b67ef8727efbdd7a
Ellamir <tamara.fendez@gmail.com>
2020-06-14 20:19:12 -0400

    devs

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import router from './router/router'
+import store from './store/store'
+import firebase from 'firebase';
+import { firebaseConfig } from "./config/firebaseConfig";
+import 'bootstrap';
+
+firebase.initializeApp(firebaseConfig);
+export const db = firebase.firestore();
 
 Vue.config.productionTip = false
 
