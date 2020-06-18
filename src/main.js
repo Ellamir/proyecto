f35b1d00a6751f8be892b8aa7ef304f40a9d75c7
anasu <misushima@gmail.com>
2020-06-18 12:46:12 -0400

    Se enstala bootstrap vue

--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ import store from './store/store'
 import firebase from 'firebase';
 import { firebaseConfig } from "./config/firebaseConfig";
 import 'bootstrap';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 firebase.initializeApp(firebaseConfig);
 export const db = firebase.firestore();
