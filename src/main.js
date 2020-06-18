d60fc705a4f45c9c630e4986a3a55aee6fd6abe8
anasu <misushima@gmail.com>
2020-06-18 17:59:03 -0400

    Correcciones generales del proyecto

--- src/main.js
+++ src/main.js
@@ -2,16 +2,14 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router/router';
 import store from './store/store';
-//import './registerServiceWorker';
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-
-import 'bootstrap/dist/css/bootstrap.css';
 
 import firebase from 'firebase';
 import { firebaseConfig } from "./config/firebaseConfig";
 
 import 'bootstrap';
-import 'bootstrap-vue';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 
 firebase.initializeApp(firebaseConfig);
