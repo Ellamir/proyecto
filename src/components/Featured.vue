16a4d411511e2bdc95e829dae8d7ddd3b619936f
anasu <misushima@gmail.com>
2020-06-19 01:13:15 -0400

    Se agregan estilos para subrayado de Titulo de seccion

--- src/components/Featured.vue
+++ src/components/Featured.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="container my-5">
         <h2>Featured</h2>
+        <div class="cuadroNaranjo"></div>
         <b-card-group deck>
             <b-card v-for="(item, index) in gameArray" :key="index"
                 :title="item.name"
@@ -33,3 +34,21 @@ export default {
 }
 </script>
 
+<style lang="scss" scoped>
+h2 {
+    border-bottom: 1px solid #727272;
+    margin-bottom: 0;
+}
+.cuadroNaranjo {
+    margin-bottom: 1rem;
+    
+    &:after{
+        content: " ";
+        display: block;
+        height: 6px;
+        max-width: 55px;
+        background: #EC8E4C;
+    }
+}
+
+</style>
