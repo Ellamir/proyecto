764390adaa6b6a94c779d5ecb5501ba27aa8ffd6
anasu <misushima@gmail.com>
2020-06-19 00:11:18 -0400

    Se termina hero section. Se deja por ahora sin texto.

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,12 +1,9 @@
 <template>
-    <div>
+    <div class="container">
         <b-carousel
             id="carousel-1"
             v-model="slide"
             :interval="4000"
-            controls
-            indicators
-            background="#ababab"
             img-width="1024"
             img-height="480"
             style="text-shadow: 1px 1px 2px #333;"
@@ -14,63 +11,25 @@
             @sliding-end="onSlideEnd"
         >
         <!-- Text slides with image -->
-        <div>
             <b-carousel-slide
-                caption="First slide"
-                text="Nulla vitae elit libero, a pharetra augue mollis interdum."
-                img-src="https://picsum.photos/1024/480/?image=52"
-            ></b-carousel-slide>
-        </div>
-
-        <!-- Slides with custom text -->
-        <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
-            <h1>Hello world!</h1>
-        </b-carousel-slide>
-
-        <!-- Slides with image only -->
-        <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
-
-        <!-- Slides with img slot -->
-        <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-        <b-carousel-slide>
-            <template v-slot:img>
-            <img
-                class="d-block img-fluid w-100"
-                width="1024"
-                height="480"
-                :src="apiConsulta.image_url"
-                alt="image slot"
+                :img-src="randomGame.images.original"
+                class="heroSlide"
             >
-            </template>
-        </b-carousel-slide>
-
-        <!-- Slide with blank fluid image to maintain slide aspect ratio -->
-        <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
-            a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
-            </p>
-        </b-carousel-slide>
+            </b-carousel-slide>
         </b-carousel>
-
-        <p class="mt-4">
-            Slide #: {{ slide }}<br>
-            Sliding: {{ sliding }}
-        </p>
     </div>
 </template>
 
 <script>
-import {random} from '../dbDummy'
+import { mapGetters } from 'vuex';
 
 export default {
     name: 'Hero',
-
     data() {
       return {
+        randomGame: {},
         slide: 0,
-        sliding: null,
-        apiConsulta: random.game,
+
       }
     },
     methods: {
@@ -84,7 +43,23 @@ export default {
       },
     },
     computed: {
-        
+        ...mapGetters([
+          'showHero'
+        ])
+    },
+    beforeMount: function () {
+      this.$store.dispatch('callDataToHero');
+      this.randomGame = this.$store.getters.showHero;
+      console.log(`this is the random Game:`, this.randomGame);
+
     }
   }
 </script>
+
+<style lang="scss" scoped>
+.heroSlide {
+  margin: 5vh 0;
+  height: 45vh;
+}
+
+</style>
