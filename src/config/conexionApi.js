f112582674e72a3c454cd4215d9de7228e5b8cb5
Ellamir <tamara.fendez@gmail.com>
2020-06-19 20:09:05 -0400

    axios fix

--- src/config/conexionApi.js
+++ src/config/conexionApi.js
@@ -1,12 +1,28 @@
 
 import axios from 'axios';
-import store from '../store/store';
 
-export function conexionApi() {
-    axios.get('https://www.boardgameatlas.com/api/search?client_id=3Xe9zqKAxF&name=labyrinth') // &string
-    .then(response=>{
-        store.dispatch('recibidoInfojuego',response.data.data);
-    }).catch(error => {
-        console.error(error);
-    })
+// import store from '../store/store';
+
+// export function conexionApi() {
+//     axios.get('https://www.boardgameatlas.com/api/search?client_id=3Xe9zqKAxF&name=labyrinth') // &string
+//     .then(response=>{
+//         store.dispatch('recibidoInfojuego',response.data.data);
+//     }).catch(error => {
+//         console.error(error);
+//     })
+// }
+
+export async function apiCall(parameters) 
+{
+    let api = 'https://www.boardgameatlas.com/api/search?client_id=3Xe9zqKAxF&';
+    try 
+    {
+        const res = await axios.get(
+            `${api}${parameters}`);
+        return res
+    } 
+    catch (error) 
+    {
+        console.log(error);
+    }
 }
