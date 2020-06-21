5d166d8ff26779af1fccd7325d9336d0317a4af3
Ellamir <tamara.fendez@gmail.com>
2020-06-20 20:52:06 -0400

    email/user de firebase y pone gravatar (en method)

--- src/views/Tam.vue
+++ src/views/Tam.vue
@@ -1,26 +1,76 @@
 <template>
     <div>
-        <div id="franjalogin" class="morado text-white">
-        PROFILE
-        Aqui va lo del perfil
-        </div>
-        <featured></featured>
+        <div id="franjasaludo" class="morado text-white">
         
+        <p @click="saludo"> Saludar en el console log </p>
+        <h1 class="hola" v-html="hola"></h1>
+        <img :src="gravatarURL" alt="algo">
+        </div>
     </div>
+    
+    <!-- <featured></featured> -->
+        
+    
 </template>
 
 <script>
-import Featured from '../components/Featured';
+// import Featured from '../components/Featured';
+import firebase from 'firebase';
+import md5 from 'js-md5';
+//protected $appends = ['gravatar'];
 
 
 export default {
+    
     name: 'Tam',
+
+    data() {
+            return {
+            //gravatarURL: 'https://es.gravatar.com/avatar/HASH-STRING?s=200&d=mp'   
+            gravatarURL: 'https://www.gravatar.com/avatar/00000000000000000000000000000000?s=200&d=mp',  //fantasma default
+            hola: ''
+            }
+    },
+
     components: {
         
-        Featured,
+        // Featured,
         
-    }
+    },
+    
+    methods: {
+    saludo(){
+            var user = firebase.auth().currentUser;
+            console.log('desde el metodo',user.displayName);
+            console.log(user.email);
+            var gravatar = md5(user.email);
+            console.log('email en md5',gravatar);
+            let URL = "https://secure.gravatar.com/avatar/"+gravatar+"?s=200&d=mp";
+            this.gravatarURL=URL;
+            let nombre = (user.displayName)  // capitalizar nombre depende de CSS o arreglar el input
+            this.hola="Hola "+nombre;
+            }
+ 
+    },
+    
+    //     filters:{
+    //  upper(str){
+    //    str = str.toLowerCase().split(' ');
+
+    //    let final = [ ];
+
+    //     for(let  word of str){
+    //       final.push(word.charAt(0).toUpperCase()+ word.slice(1));
+    //     }
+
+    //   return final.join(' ')      
+    //     }
+    //   },
+        
+
+    
 }
+
 </script>
 
 <style lang="scss" scoped>
@@ -33,4 +83,6 @@ export default {
 .mejorlogin {
           text-decoration-color: #FFF;
         }
+
+// .hola { text-transform: capitalize; }
 </style> 
