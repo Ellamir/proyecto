254ddc51be5075820d1465eb771dc06efadb26ea
Anasu <misushima@gmail.com>
2020-06-23 13:23:11 -0400

    Se agrega checkeo interno para no duplicar juegos en favoritos

--- src/store/store.js
+++ src/store/store.js
@@ -5,113 +5,117 @@ import { apiCall } from '../config/conexionApi'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    randomHero: {
-      images: {
-        original: '',
-      }
+    state: {
+        randomHero: {
+            images: {
+                original: '',
+            }
+            },
+            randomFeature: [],
+            featuredQuantity: 4,
+            popularToday:{},
+            myFavs:[],
     },
-    randomFeature: [],
-    featuredQuantity: 4,
-    popularToday:{},
-    myFavs:[],
-  },
-  getters: {
-    showHeroIMG(state) 
-    {
-      return state.randomHero.images.original
-    },
-    showFeature(state)
-    {
-      return state.randomFeature;
-    },
-    showPopular(state)
-    {
-      return state.popularToday;
-    },
-    showFavs(state)
-    {
-      return state.myFavs;
-    },
-
-  },
-  mutations: {
-    dataToRandomHero(state,random) 
-    {
-      state.randomHero = random;
-    },
-    dataToRandomFeature(state,randomGame) 
-    {
-      state.randomFeature.push(randomGame);
-    },
-    clearFeatured(state)
-    {
-      state.randomFeature = [];
-    },
-    dataToPopularToday(state,popularGame)
-    {
-      state.popularToday = popularGame;
-    },
-    addToFavorites(state, game)
-    {
-      state.myFavs.push(game);
-    }
-  },
-  actions: {
-    callDataToHero(context) 
-    {
-      let callRandom = 'random=true';
-
-      async function apiRes () {
-        const getRandom = await (
-          await apiCall(callRandom)
-        ).data.game;
-        context.commit('dataToRandomHero', getRandom);
-      }
-      apiRes()
-    },
-    callDataToFeature(context)
-    {
-      let callRandom = 'random=true';
-      context.commit('clearFeatured')
-
-      const apiRes = async () => {
-        for(let i = 0; i < 4; i++) 
+    getters: {
+        showHeroIMG(state) 
         {
-          const getRandom = await (
-            await apiCall(callRandom)
-          ).data.game;
-          context.commit('dataToRandomFeature',getRandom);
+            return state.randomHero.images.original
+        },
+        showFeature(state)
+        {
+            return state.randomFeature;
+        },
+        showPopular(state)
+        {
+            return state.popularToday;
+        },
+        showFavs(state)
+        {
+            return state.myFavs;
+        },
+        isFavorite(state)
+        {
+            return gameID => 
+            {
+                return state.myFavs.includes(gameID);
+            }
+        },
+    },
+    mutations: {
+        dataToRandomHero(state,random) 
+        {
+            state.randomHero = random;
+        },
+        dataToRandomFeature(state,randomGame) 
+        {
+            state.randomFeature.push(randomGame);
+        },
+        clearFeatured(state)
+        {
+            state.randomFeature = [];
+        },
+        dataToPopularToday(state,popularGame)
+        {
+            state.popularToday = popularGame;
+        },
+        addToFavorites(state, gameID)
+        {
+            state.myFavs.push(gameID);
         }
-      }
-      apiRes()
     },
-    callPopularToday(context)
-    {
-      let callPopular = 'order_by=popularity';
+    actions: {
+        callDataToHero(context) 
+        {
+            let callRandom = 'random=true';
 
-      const apiRes = async () => {
-        const getPopular = await (
-          await apiCall(callPopular)
-        ).data.games[0];
-        context.commit('dataToPopularToday', getPopular);
-      }
+            async function apiRes () {
+                const getRandom = await (
+                    await apiCall(callRandom)
+                ).data.game;
+                context.commit('dataToRandomHero', getRandom);
+            }
+            
+            apiRes()
+        },
+        callDataToFeature(context)
+        {
+            let callRandom = 'random=true';
+            context.commit('clearFeatured')
 
-      apiRes();
-    },
-    callFavorites(context,id)
-    {
-      let selectedGame = `ids=${id}`;
-      // la llamada es parte de otra funcion.
-      // Pasar selectedGame directo como un string al arreglo
-      const apiRes = async () => {
-        const getFav = await (
-          await apiCall(selectedGame)
-        ).data.games[0];
-        context.commit('addToFavorites', getFav);
-      }
+            const apiRes = async () => {
+                for(let i = 0; i < 4; i++) 
+                {
+                    const getRandom = await (
+                        await apiCall(callRandom)
+                    ).data.game;
+                    context.commit('dataToRandomFeature',getRandom);
+                }
+            }
+            apiRes()
+        },
+        callPopularToday(context)
+        {
+            let callPopular = 'order_by=popularity';
 
-      apiRes();
+            const apiRes = async () => {
+                const getPopular = await (
+                    await apiCall(callPopular)
+                ).data.games[0];
+                context.commit('dataToPopularToday', getPopular);
+            }
+
+            apiRes();
+        },
+        callFavorites(context,id)
+        {
+            if(!this.getters.isFavorite(id))
+            {
+                context.commit('addToFavorites', id);
+            }
+            else
+            {
+                console.log(`El ID ${id} ya está registrado`)
+            }
+        },
     }
-  } 
 })
